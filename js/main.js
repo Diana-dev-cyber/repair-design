701bb102a6366e15afe6778f4f38f6053b4a4427
Diana-dev-cyber <din_mak96@mail.ru>
2019-12-22 19:35:30 +0300

    feat: add css animate

--- js/main.js
+++ js/main.js
@@ -29,21 +29,25 @@ $(document).ready(function () {
   });
 
   var mySwiper = new Swiper('.swiper-container', {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    })
-   
-    var next = $('.swiper-button-next');
-    var prev = $('.swiper-button-prev');
-    var bullets = $('.swiper-pagination');
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
 
-    next.css('left', prev.width() + 10 + bullets.width() + 10);
-    bullets.css('left', prev.width() + 10);
-  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10);
+
+  new WOW().init();
+  // prev.css('center', next.width() + 10 + bullets.width() + 10);
+  // prev.css('center', bullets.width() + 10);
+});
