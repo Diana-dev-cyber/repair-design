e00cb4b6a5bf5da79e29f818d98cfc6b7c72d574
Diana-dev-cyber <din_mak96@mail.ru>
2019-12-25 17:03:42 +0300

    feat: fail send.php, phpmailer and thanks.html

---
+++ send.php
@@ -0,0 +1,41 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  //Server settings
+  $mail->SMTPDebug = 0;                      // Enable verbose debug output
+  $mail->isSMTP();                                            // Send using SMTP
+  $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+  $mail->Username   = 'mak205468@gmail.com';                     // SMTP username
+  $mail->Password   = '379973Din';                               // SMTP password
+  $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+  $mail->Port       = 465;                                    // TCP port to connect to
+  $mail->CharSet = 'utf-8'; 
+
+  //Recipients
+  $mail->setFrom('mak205468@gmail.com', 'Диана');
+  $mail->addAddress('mak205468@gmail.com');     // Add a recipient
+
+  // Content
+  $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = 'Новая заявка с сайта';
+  $mail->Body    = "Имя пользователя: ${userName}, Его телефон: ${userPhone}, Его email: ${userEmail}";
+
+  $mail->send();
+  header('Location: thanks.html');
+} catch (Exception $e) {
+  echo "Письмо не отправленно, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
